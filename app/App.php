de314ac701112bb5cf43ccfd36757aff8963f897
ahmed <boutoualineahmed@gmail.com>
2025-09-10 06:28:36 +0100

    add function read files

--- app/App.php
+++ app/App.php
@@ -3,7 +3,22 @@
 declare(strict_types = 1);
 
 // Your Code
+    function gettransactionfile( string $dirpath): array {
+        $files = [];
+        foreach (scandir($dirpath) as $file){
+            if (is_dir($file)){
+                continue;
+            }
+            $files[]= FILES_PATH . $file;
+        }
+        return $files;
+    }
 
+
+
+
+
+?>
 
 
 
